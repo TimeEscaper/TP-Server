7274bfe66dc5f6db8840555b0da18c7f46400a06
TimeEscaper <rentgeny05@gmail.com>
2017-03-29 03:04:52 +0300

    ClientHandler basic send and receive

--- include/server/ClientHandler.h
+++ include/server/ClientHandler.h
@@ -1,8 +1,9 @@
 #ifndef TP_SERVER_CLIENTHANDLER_H
 #define TP_SERVER_CLIENTHANDLER_H
 
-
 #include <cstddef>
+
+#define DEFAULT_REQUEST_BUFFER 1024
 
 class ClientHandler {
 private:
@@ -12,8 +13,8 @@ public:
     ClientHandler(int socket);
     ~ClientHandler();
     int getSocket();
-    void send(const char* data);
-    void receive(char* data);
+    void sendRaw(const char* data);
+    char* receiveRaw();
 };
 
 
--- src/server/ClientHandler.cpp
+++ src/server/ClientHandler.cpp
@@ -1,1 +1,39 @@
+#include <unistd.h>
+#include <sys/socket.h>
 #include "../../include/server/ClientHandler.h"
+#include "../../include/helpers/utils.h"
+
+ClientHandler::ClientHandler(int socket) {
+    this->socket = socket; //TODO: check on < 0
+}
+
+ClientHandler::~ClientHandler() {
+    if (socket > 0) {
+        close(socket);
+    }
+}
+
+void ClientHandler::sendRaw(const char* data) {
+    size_t dataSize = sizeof(data);
+    size_t left = sizeof(data);
+    ssize_t sent = 0;
+    while (left > 0) {
+        sent = ::send(socket, data + sent, dataSize - sent, 0);
+        if (sent == -1) {
+            utils::log("Unable to send data!");
+            return;
+        }
+        left -= sent;
+    }
+}
+
+char* ClientHandler::receiveRaw() {
+    size_t bufferSize = DEFAULT_REQUEST_BUFFER; //TODO: may be moved to private field
+    char* buffer = new char[bufferSize];
+    size_t read = recv(socket, buffer, bufferSize, 0); //Blocking
+    if (read<0) {
+        delete[] buffer;
+        return NULL;
+    }
+    return buffer;
+}
